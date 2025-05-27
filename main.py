749ceb9b97521fdce5c7004d4cc4b195cef9ee82
AYUSH SHARMA <aayushparashar984@gmail.com>
2025-05-27 13:16:04 +0530

    Add the dummy json data
    Apply the get https methord GET and create the view api

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
+import json
 
+#function to retrive the data 
+def load_data():
+    with open('patient.json','r') as file:
+        data=json.load(file)
+    return data
 #creating the demo api server
 
 app=FastAPI()
 
-@app.get("/")
+@app.get('/')
 def hello():
-    return {'message':'Hello world '}
-    print('Hello World')
+    return {'message':'Welcome to Patient Management System Usig Fast API '}
+    
+#creating the about 
+@app.get('/about')
+def about():
+    return{'message':'This is the fully functional API for the Patient Management Application'}
+
+#creating the view page
+@app.get('/view')
+def view():
+    data=load_data()
+    return data
